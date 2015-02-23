5470b7e7a616e84b889d2be06feb20f180fe0a29
Baggers <chris.bagley@gmail.com>
2015-02-23 21:06:03 +0100

    bug in partial apply

--- fn_.lisp
+++ fn_.lisp
@@ -27,21 +27,21 @@
 
 (defun %-arg-num (arg)
   (let ((name (symbol-name arg)))
-    (if (= (length name) 1) 
+    (if (= (length name) 1)
         0
         (parse-integer (subseq name 1)))))
 
 (defun sequential (nums &optional (base 0))
   (cond ((null nums) t)
         ((and base (not (find base nums))) nil)
-        (t (not (null (reduce (lambda (x y) (and x y (if (= (- y x) 1) y nil))) 
+        (t (not (null (reduce (lambda (x y) (and x y (if (= (- y x) 1) y nil)))
                               nums))))))
 
 (defmacro fn% (&body body)
   (let* ((raw-args (get-%-symbs body))
          (at-form (find "%@" raw-args :key #'symbol-name :test #'equal))
          (raw-args (remove "%@" raw-args :key #'symbol-name :test #'equal))
-         (args (sort (remove-duplicates raw-args) 
+         (args (sort (remove-duplicates raw-args)
                      (lambda (x y) (< (%-arg-num x) (%-arg-num y))))))
     (if (sequential (sort (remove-duplicates (mapcar #'%-arg-num args)) #'<))
         (if at-form
@@ -54,7 +54,7 @@
 
 ;; Partial Application
 ;; -------------------
-;; {TODO} Need to profule multiple-value-call to see if it is any better 
+;; {TODO} Need to profule multiple-value-call to see if it is any better
 ;;        than append
 
 (defun fn_ (function &rest args)
@@ -63,16 +63,17 @@
            (function function))
   (lambda (&rest rest-of-the-args)
      (multiple-value-call function
-       (values-list args) 
+       (values-list args)
        (values-list rest-of-the-args))))
 
 (define-compiler-macro fn_ (function &rest args)
-  `(let ((function ,function))
-     (declare (optimize (speed 3) (safety 1) (debug 1))
-              (function function))
-     (lambda (&rest rest-of-the-args)
-        (declare (optimize (speed 3) (safety 1) (debug 1)))
-        (apply ,@args rest-of-the-args))))
+  (let ((gfunc (gensym "function")))
+    `(let ((,gfunc ,function))
+       (declare (optimize (speed 3) (safety 1) (debug 1))
+                (function ,gfunc))
+       (lambda (&rest rest-of-the-args)
+         (declare (optimize (speed 3) (safety 1) (debug 1)))
+         (apply ,gfunc ,@args rest-of-the-args)))))
 
 (defun fn_r (function &rest args)
   "Partially apply args to function"
@@ -119,7 +120,7 @@ and then calling the next one with the primary value of the last."
 
 (defun lambda-reader (stream char)
   (declare (ignore char))
-  (let* ((body (read stream t nil t))         
+  (let* ((body (read stream t nil t))
          (body (if (and (= 1 (length body))
                         (or (and (symbolp (first body))
                                (or (equal (symbol-name (first body)) "%")
